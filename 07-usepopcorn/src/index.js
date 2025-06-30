cd9d8a7098894c078acd96274f91789d187cddb2
ibrahim Ali <ibrahimali1w411@gmail.com>
2025-06-30 09:47:08 +0300

    finish section 10 - learnign about comonents composition and reusability

---
+++ 07-usepopcorn/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+import StarRating from './starRating';
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={8} color="blue" onSetRaing={setMovieRating} />
+      <p>this movie was rated {movieRating} stars </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+
+    <StarRating
+      maxRating="5"
+      messages={['Terrible', 'Bad', 'Nice', 'Good', 'Amazing']}
+    />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={25}
+      className="test"
+      defaultRaing={4}
+    />
+    <Test />
+  </React.StrictMode>
+);
