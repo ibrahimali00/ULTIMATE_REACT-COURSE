409c928343ac156212bcaf0d8c3db6ccf7e6c023
ali <ebrahimali0093@gmail.com>
2026-03-09 09:10:58 +0200

    add section about redux , modern redux toolkit and difference between redux and context api + reducer

---
+++ redux-intro/src/features/accounts/accoutSlice.js
@@ -0,0 +1,134 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+  isLoading: false,
+};
+
+const accoutSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    deposite(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+
+      reducer(state, action) {
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = '';
+    },
+
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accoutSlice.actions;
+
+export function deposite(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposite', payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: 'account/convertingCurrency' });
+    // API Call
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    // return action
+
+    dispatch({ type: 'account/deposite', payload: converted });
+  };
+}
+
+export default accoutSlice.reducer;
+
+/*
+
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case 'account/deposit':
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
+    case 'account/withdraw':
+      return { ...state, balance: state.balance - action.payload };
+    case 'account/requestLoan':
+      if (state.loan > 0) return state;
+      // LATER
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case 'account/payLoan':
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
+      };
+
+    case 'account/convertingCurrency':
+      return { ...state, isLoading: true };
+    default:
+      return state;
+  }
+}
+
+export function deposite(amount, currency) {
+  if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: 'account/convertingCurrency' });
+    // API Call
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?amount=${amount}&from=${currency}&to=USD`,
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+
+    // return action
+
+    dispatch({ type: 'account/deposit', payload: converted });
+  };
+}
+export function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+export function requestLoan(loanAmount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount: loanAmount, purpose: purpose },
+  };
+}
+export function payLoan() {
+  return { type: 'account/payLoan' };
+}
+  */
---
+++ redux-intro/src/store.js
@@ -0,0 +1,12 @@
+import accountReducer from './features/accounts/accoutSlice';
+import customerReducer from './features/customers/customerSlice';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
+
+export default store;
