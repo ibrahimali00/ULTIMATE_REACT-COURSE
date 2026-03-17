52ddb8fda4b79c7319bdde588c28712f81d2c4d9
ali <ebrahimali0093@gmail.com>
2026-03-17 14:07:21 +0200

    add state using redux toolkit and learn some advanced concepts as createAsyncThunk and useFetcher

--- 15-fast-recat-pizza/src/features/user/CreateUser.jsx
+++ 15-fast-recat-pizza/src/features/user/CreateUser.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { updateName } from './userSlice';
 
 function CreateUser() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
